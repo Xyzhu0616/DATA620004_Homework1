9145dda360cd9bfd52b5141f4d63afbc244cc040
Xyzhu0616 <66372105+Xyzhu0616@users.noreply.github.com>
2022-03-27 20:21:38 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+from importlib.util import LazyLoader
+import os
+import struct
+import numpy as np
+import matplotlib.pyplot as plt 
+
+import twolayer 
+
+
+loadnet =twolayer.TwoLayerNet()
+test_image,test_label = twolayer.load_mnist('./data','t10k')
+filename = './save_model.npz'
+loadnet.load_model(filename)
+
+loadnet_accuracy = (loadnet.predict(test_image) == test_label).mean()
+print('Accuracy:',loadnet_accuracy)
